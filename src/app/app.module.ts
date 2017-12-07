c2c7d51c5bfd8cdc941efdf31d853764ce137108
Pure <2419281635@qq.com>
2017-12-07 15:09:55 +0800

    完成app.module的copy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {CoreModule} from './@core/core.module';
 
 
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ThemeModule} from './@theme/theme.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    CoreModule.forRoot(),
+    ThemeModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
