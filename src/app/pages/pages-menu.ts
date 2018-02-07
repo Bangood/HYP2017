2c712643f6fe0d58f4382121e87dbc3fbf4da6d8
Pure <2419281635@qq.com>
2018-02-07 15:27:55 +0800

    添加pages-menu

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,55 @@
+/**
+ * Created by pure on 2018/2/7.
+ */
+//
+//
+import {NbMenuItem} from '@nebular/theme';
+export const MENU_ITEM: NbMenuItem[] = [
+  {
+    title: 'Dashboard',
+    icon: 'nb-home',
+    link: '/pages/dashboard',
+    home: true
+  }, {
+    title: 'FEATURES',
+    group: true
+  }, {
+    title: 'Herbs',
+    icon: 'nb-keypad',
+    link: '/pages/herbs',
+    children: [
+      {
+        title: 'Add',
+        link: '/pages/herbs/add',
+      }, {
+        title: 'List',
+        link: '/pages/herbs/list'
+      }
+    ],
+  }, {
+    title: '种族',
+    icon: 'nb-keypad',
+    link: '',
+    children: []
+  }, {
+    title: '势力',
+    icon: 'nb-keypad',
+    link: '',
+    children: []
+  }, {
+    title: '宗教',
+    icon: 'nb-keypad',
+    link: '',
+    children: []
+  }, {
+    title: '群芳谱',
+    icon: 'nb-keypad',
+    link: '',
+    children: []
+  }, {
+    title: '红颜楼',
+    icon: 'nb-keypad',
+    link: '',
+    children: []
+  },
+];
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -2,13 +2,18 @@
  * Created by pure on 2018/2/2.
  */
 import {Component} from '@angular/core';
+//
+//
+import {MENU_ITEM} from './pages-menu';
 @Component({
   selector: 'hyp-pages',
   template: `
     <hyp-sample-layout>
+      <nb-menu [items]="menu"></nb-menu>
       <router-outlet></router-outlet>
     </hyp-sample-layout>
   `
 })
 export class PagesComponent {
+  menu = MENU_ITEM;
 }
